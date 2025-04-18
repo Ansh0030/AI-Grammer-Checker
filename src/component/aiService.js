4c554e992d6d4303df6d79ae43f6dfdd42713405
Ansh Kumar <ansh.kumar@mpccloudconsulting.com>
2025-04-18 10:33:14 +0530

    first commit

--- src/component/aiService.js
+++ src/component/aiService.js
@@ -5,15 +5,16 @@ export const submitPrompt = async (text) => {
 
   const headers = {
     "Content-Type": "application/json",
-    // Add Autorization Key into this to call API
+    Authorization:
+      "Bearer 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",
   };
 
   const data = {
-    model: "gpt-4.1-nano",
+    model: "gpt-4.1-nano", // Or "gpt-4", "gpt-3.5-turbo"
     messages: [
       { role: "user", content: `Improve grammer of this text ${text}` },
     ],
-    stream: false,
+    stream: false, // optional
   };
 
   try {
